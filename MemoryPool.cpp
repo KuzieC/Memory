ab259ce55d6a576ed35bbf984490323fc0b3461d
Kuzie <kuziechen@outlook.com>
2025-05-08 19:43:37 +0100

    Refactor MemoryPool: update allocate method to include alignment parameter and add alignment padding function

--- MemoryPool.cpp
+++ MemoryPool.cpp
@@ -1,11 +1,39 @@
 #include "MemoryPool.h"
 
-void* MemoryPool::allocate(size_t n) {
-    //request new block if not available 
 
-    //link fristBlock pointer
+size_t MemoryPool::alighPad(char* cur, size_t align) {
+    return (align - (reinterpret_cast<uintptr_t>(cur) % align))%align;
+}
+void* MemoryPool::allocate(size_t n, size_t align) {
+    //request new block if not available 
+    if(!currBlock || currBlock >= endBlock) {
+        Block* newBlock = static_cast<Block*>(operator new(BLOCK_SIZE));
+        //link fristBlock pointer
+        newBlock->next = firstBlock;
+        firstBlock = newBlock;   
+        char* newCurr = reinterpret_cast<char*>(newBlock) + sizeof(Block);
+        size_t padding = alighPad(newCurr, align);
+        currBlock = reinterpret_cast<uint8_t*>(newCurr + padding);
+        endBlock = reinterpret_cast<uint8_t*>(reinterpret_cast<char*>(newBlock) + BLOCK_SIZE - SLOT_SIZE + 1);
+    }
+
+    void* ret = currBlock;
+
+    char* newCurr = reinterpret_cast<char*>(currBlock) + n;
+    size_t padding = alighPad(newCurr, align);
+    currBlock = reinterpret_cast<uint8_t*>(newCurr + padding); //move curr pointer
+
+    return ret; //return the pointer to the allocated memory
 
     //move curr and end pointer
 
     //add padding and move curr
+}
+
+void MemoryPool::deallocate() {
+    while(firstBlock) {
+        auto next = firstBlock->next;
+        operator delete(firstBlock);
+        firstBlock = next;
+    }
 }
--- MemoryPool.h
+++ MemoryPool.h
@@ -1,14 +1,21 @@
 #pragma once
 
+#include <cstdint>
 struct Block {
     Block* next;
 };
 
+static const size_t BLOCK_SIZE = 4096;
+static const size_t SLOT_SIZE = 8;
 class MemoryPool{
-    Block* currBlock;
-    Block* endBlock;
-    Block* firstBlock;
+    uint8_t* currBlock = nullptr;
+    uint8_t* endBlock = nullptr;
+    Block* firstBlock = nullptr;
 
-    void* allocate(size_t n);
+    void* allocate(size_t n, size_t align);
     void deallocate();
+    size_t alighPad(char* cur, size_t align);
+    ~MemoryPool() {
+        deallocate();
+    }
 };
