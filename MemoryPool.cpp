c4b6c8387cd05f3a94fad4b56466d9873b481508
Kuzie <kuziechen@outlook.com>
2025-05-12 16:21:10 +0100

    Modify to work with hashBucket

--- MemoryPool.cpp
+++ MemoryPool.cpp
@@ -1,20 +1,61 @@
 #include "MemoryPool.h"
 
+MemoryPool::MemoryPool()
+    : SLOT_SIZE(0)
+    , freeList(nullptr)
+    , currBlock(nullptr)
+    , endBlock(nullptr)
+    , firstBlock(nullptr){
 
+}
+
+MemoryPool::~MemoryPool(){
+    deallocateAll();
+}
+
+void MemoryPool::init(size_t size) {
+    SLOT_SIZE = size;
+    firstBlock = nullptr;
+    currBlock = nullptr;
+    endBlock = nullptr;
+    freeList = nullptr;
+}
 size_t MemoryPool::alighPad(char* cur, size_t align) {
     return (align - (reinterpret_cast<uintptr_t>(cur) % align))%align;
 }
+
+void* MemoryPool::popFreeList() {
+    if( freeList == nullptr) return nullptr;
+    void* ret = freeList;
+    freeList = freeList->next;
+    return ret;
+}
+
+void MemoryPool::pushFreeList(Slot* b) {
+    b->next = freeList;
+    freeList = b;
+}
+
 void* MemoryPool::allocate(size_t n, size_t align) {
-    //check if we can fit n in the current block
+    //check if we can fit n in the current Slot
+    if( n > BLOCK_SIZE - sizeof(SLOT_SIZE)) {
+        return operator new(n);
+    }
+    void* newFreeBlock = popFreeList();
+    if(newFreeBlock != nullptr){
+        return newFreeBlock;
+    }
+
     if(!currBlock || currBlock + n >= endBlock) {
-        Block* newBlock = static_cast<Block*>(operator new(BLOCK_SIZE));
+        Slot* newBlock = static_cast<Slot*>(operator new(BLOCK_SIZE));
         //link fristBlock pointer
         newBlock->next = firstBlock;
         firstBlock = newBlock;   
-        char* newCurr = reinterpret_cast<char*>(newBlock) + sizeof(Block);
+        char* newCurr = reinterpret_cast<char*>(newBlock) + sizeof(Slot);
         size_t padding = alighPad(newCurr, align);
         currBlock = newCurr + padding;
         endBlock = reinterpret_cast<char*>(newBlock) + BLOCK_SIZE;
+        //freeList = nullptr;
     }
 
     void* ret = currBlock;
@@ -30,7 +71,12 @@ void* MemoryPool::allocate(size_t n, size_t align) {
     //add padding and move curr
 }
 
-void MemoryPool::deallocate() {
+void MemoryPool::deallocate(Slot* b) {
+    if(!b) return;
+    pushFreeList(b);
+}
+
+void MemoryPool::deallocateAll() {
     while(firstBlock) {
         auto next = firstBlock->next;
         operator delete(firstBlock);
--- MemoryPool.h
+++ MemoryPool.h
@@ -1,22 +1,40 @@
 #pragma once
 
 #include <cstdint>
-struct Block {
-    Block* next;
+struct Slot {
+    Slot* next;
 };
 
 static const size_t BLOCK_SIZE = 4096;
-static const size_t SLOT_SIZE = 8;
+//static const size_t SLOT_SIZE = 8;
 class MemoryPool{
-    char* currBlock = nullptr;
-    char* endBlock = nullptr;
-    Block* firstBlock = nullptr;
+    Slot* freeList;
+    char* currBlock;
+    char* endBlock;
+    Slot* firstBlock;
+    size_t SLOT_SIZE;
     size_t alighPad(char* cur, size_t align);
-public:
-    void* allocate(size_t n, size_t align);
-    void deallocate();
+    void* popFreeList();
+    void pushFreeList(Slot* b);
 
-    ~MemoryPool() {
-        deallocate();
-    }
+public:
+    void* allocate(size_t n, size_t align = 8);
+    void deallocate(Slot* b);
+    void deallocateAll();
+    void init(size_t Slot_size);
+    MemoryPool();
+    ~MemoryPool();
 };
+
+/**
+Standard operator new: 68502 ms   
+Custom memory pool: 1086 ms       
+
+[Small Object Test]
+Standard operator new: 75800 ms   
+Custom memory pool: 275 ms        
+
+[Large Object Test]
+Standard operator new: 123478 ms  
+Custom memory pool: 86214 ms
+ */
