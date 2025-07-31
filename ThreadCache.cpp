1e9cf8e9d0f5df2795f6f32a5b61e78cd27ee53c
Kuzie <99990014+KuzieC@users.noreply.github.com>
2025-07-31 22:45:09 +0100

    Fix returnCentralCache loop to retain correct nodes

--- ThreadCache.cpp
+++ ThreadCache.cpp
@@ -45,13 +45,13 @@ void ThreadCache::returnCentralCache(void* ptr, size_t size){
     size_t keptNum = totalNum / 4;
     
     void* end = ptr;
-    for(size_t i = 0; i < keptNum; ++i){
+    for (size_t i = 1; i < keptNum; ++i) {
         end = *reinterpret_cast<void**>(end);
     }
 
     void* returnPtr = *reinterpret_cast<void**>(end);
     *reinterpret_cast<void**>(end) = nullptr;
-    freeList[index] = end;
+    freeList[index] = ptr;
     freeListSize[index] = keptNum;
     if(returnPtr){
         // Return the rest of the memory to the central cache
